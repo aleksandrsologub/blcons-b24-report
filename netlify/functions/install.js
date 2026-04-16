5761b5167ace1a8bd6e08707747236d8702156fe
aleksandrsologub <aleksandr.sologub@gmail.com>
2026-04-16 18:25:05 +0300

    Update install.js

--- netlify/functions/install.js
+++ netlify/functions/install.js
@@ -1,4 +1,18 @@
+const fs = require("fs");
+const path = require("path");
+
 exports.handler = async (event) => {
+  const method = event.httpMethod;
+
+  if (method === "POST") {
+    const html = fs.readFileSync(path.join(__dirname, "../../index.html"), "utf8");
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "text/html; charset=utf-8" },
+      body: html
+    };
+  }
+
   return {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
